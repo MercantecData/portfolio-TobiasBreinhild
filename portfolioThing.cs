eb99ee45ed4a693d7877267bdb36a3eeb91944e3
Tobias Breinhild <tbbr.skp@edu.mercantec.dk>
2019-11-04 10:31:12 +0100

    Første Commit

--- portfolioThing.cs
+++ portfolioThing.cs
@@ -6,7 +6,7 @@ namespace portfolioThing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi Dumbass!");
         }
     }
 }
