963c767e08622a5d9218fbb9815472acaebfb18f
Tobias Breinhild <tbbr.skp@edu.mercantec.dk>
2019-11-04 10:40:14 +0100

    Tæller

--- portfolioThing.cs
+++ portfolioThing.cs
@@ -6,7 +6,18 @@ namespace portfolioThing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi Dumbass!");
+            int[] numbers = { };
+
+            int i = 0;
+
+            int input = int.Parse(Console.ReadLine());
+
+            while (i < input)
+            {
+                numbers[i] = i;
+
+                i += 1;
+            }
         }
     }
 }
