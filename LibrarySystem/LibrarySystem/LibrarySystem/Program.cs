32d839e1e71b6a3e43cee9369a507b4456f492e1
Tobias Breinhild <tbbr.skp@edu.mercantec.dk>
2019-11-04 14:10:29 +0100

    Mostly Functional Library

--- LibrarySystem/LibrarySystem/LibrarySystem/Program.cs
+++ LibrarySystem/LibrarySystem/LibrarySystem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace LibrarySystem
 {
@@ -12,44 +13,113 @@ namespace LibrarySystem
             Book horrorBook = new Book();
             Book aVeryLongHarryPotterBook = new Book();
 
+            int bookBorrowingTime = 7;
+
             kidsBook.name = "Bob and the GoKart";
             horrorBook.name = "Blood and Gore";
             aVeryLongHarryPotterBook.name = "Harry Potter and The Very Long Book";
 
             string[] books = { kidsBook.name, horrorBook.name, aVeryLongHarryPotterBook.name };
 
-            string[] borrowedBook = new string[1];
+            string[] borrowedBook = { "None" };
 
-            while(isRunning)
+            Console.WriteLine(" ");
+            Console.WriteLine("Welcome to the Library! What can I help you with?");
+            Console.WriteLine(" ");
+
+            while (isRunning)
             {
-                Console.WriteLine("Welcome to the Library! What can I help you with?");
                 Console.WriteLine(" ");
                 Console.WriteLine("1. Borrow a book");
                 Console.WriteLine("2. Return a book");
                 Console.WriteLine("3. Check for borrowed books");
-                Console.WriteLine("4. Edit the borrowing time of a book");
+                Console.WriteLine("4. Extend the borrowing time of a book");
                 Console.WriteLine("5. Exit the Library");
                 Console.WriteLine(" ");
 
                 string libraryOptions = Console.ReadLine();
+                Console.WriteLine(" ");
 
                 switch (libraryOptions)
                 {
                     case "1":
                         Console.WriteLine("Which book would you like to borrow?");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("1. Bob and the GoKart");
+                        Console.WriteLine("2. Blood and Gore");
+                        Console.WriteLine("3. Harry Potter and The Very Long Book");
+                        Console.WriteLine(" ");
+
+                        string borrowBook = Console.ReadLine();
+
+                        if (borrowBook == "1")
+                        {
+                            borrowedBook[0] = books[0];
+                        }
+                        else if (borrowBook == "2")
+                        {
+                            borrowedBook[0] = books[1];
+                        }
+                        else if (borrowBook == "3")
+                        {
+                            borrowedBook[0] = books[2];
+                        }
+
                         break;
+
                     case "2":
-                        Console.WriteLine("Which book would you like to return?");
+                        if (borrowedBook[0] == books[0])
+                        {
+                            Console.WriteLine("You have returned: " + books[0]);
+                            borrowedBook[0] = "None";
+                        }
+                        else if (borrowedBook[0] == books[1])
+                        {
+                            Console.WriteLine("You have returned: " + books[1]);
+                            borrowedBook[0] = "None";
+                        }
+                        else if (borrowedBook[0] == books[2])
+                        {
+                            Console.WriteLine("You have returned: " + books[2]);
+                            borrowedBook[0] = "None";
+                        }
+                        else
+                        {
+                            Console.WriteLine("You currently don't have any books to return");
+                        }
                         break;
+
                     case "3":
                         Console.WriteLine("Let me just check want you have borrowed");
+                        Thread.Sleep(1000);
+                        Console.Write(".");
+                        Thread.Sleep(1000);
+                        Console.Write(".");
+                        Thread.Sleep(1000);
+                        Console.WriteLine(".");
+                        Thread.Sleep(1000);
+                        Console.WriteLine(" ");
+                        Console.WriteLine("You have borrowed: " + borrowedBook[0]);
                         break;
+
                     case "4":
-                        Console.WriteLine("What should the new time be?");
+                        Console.WriteLine("How many days would you like to extend it by?");
+                        Console.WriteLine(" ");
+
+                        int extendTime = int.Parse(Console.ReadLine());
+                        bookBorrowingTime = bookBorrowingTime + extendTime;
+
+                        Console.WriteLine("The borrow time on " + borrowedBook[0] + " has been extended by " + extendTime + " days!");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("You should return " + borrowedBook[0] + " in " + bookBorrowingTime + " days!");
+                        Console.WriteLine(" ");
                         break;
+
                     case "5":
                         Console.WriteLine("See you again soon! Bye!");
+                        isRunning = false;
                         break;
+
                     default:
                         Console.WriteLine("I don't understand?!");
                         break;
