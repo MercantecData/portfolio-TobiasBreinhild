57c89a1bc98bfd96a55b0e22c6a27d6a42c0f2db
Tobias Breinhild <tbbr.skp@edu.mercantec.dk>
2019-11-06 10:41:00 +0100

    Added Bicycles and Shop Classes

---
+++ yderligereTræning/BicycleShop/BicycleShop/Bicycles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BicycleShop
+{
+    class Bicycles
+    {
+        public string brand;
+        public double wheelSize;
+        public string color;
+        public int gears;
+        public string bicycleSize;
+    }
+}
--- yderligereTræning/BicycleShop/BicycleShop/Program.cs
+++ yderligereTræning/BicycleShop/BicycleShop/Program.cs
@@ -6,7 +6,20 @@ namespace BicycleShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
+    /*
+        3. Lav en klasse for cykelbutik. Denne skal kunne indeholde et variabelt antal cykel-objekter.
+
+        4. Lav en funktion i butik-klassen. Denne skal kunne returnere alle cyklers mærke (dvs. ikke selve cykel objektet) som et string-array.
+
+        5. Lav to funktioner i butik-klassen, som begge tager imod en double.
+        Den ene skal returnere alle cykler (som et Cykel array) med hjulstørrelse under det givne,
+        og den anden skal returnere alle cykler med hjulstørrelse over det givne.
+
+        6. Lav en klasse for cykel koncernen (dvs. den som ejer alle butikkerne).
+
+        7. Lav en funktion i koncern klassen som svarer på hvor mange røde cykler der er i hele koncernen.
+    */
