02d9a04246d08a8c47d9a82b99bf006f8432caef
Tobias Breinhild <tbbr.skp@edu.mercantec.dk>
2019-11-08 09:06:00 +0100

    Added Borrowing and Returning
    I know there is a lot in the main code. Fixing it when everything works properly

--- LibrarySystem/LibrarySystem/LibrarySystem/Library.cs
+++ LibrarySystem/LibrarySystem/LibrarySystem/Library.cs
@@ -9,11 +9,28 @@ namespace LibrarySystem
         Book teenBook = new Book("Bob and Bob gets married", 100);
         Book adultBook = new Book("Bob and Bob's Lifestory", 2000);
 
+        public string[] borrowedBook = { "Nothing" };
+
         public string WelcomeMessage()
         {
             string welcome = "Welcome to Bob and Bob's Library!";
 
             return welcome;
+        }
+
+        public void BorrowBook1()
+        {
+            borrowedBook[0] = kidsBook.title;
+        }
+
+        public void BorrowBook2()
+        {
+            borrowedBook[0] = teenBook.title;
+        }
+
+        public void BorrowBook3()
+        {
+            borrowedBook[0] = adultBook.title;
         }
     }
 }
--- LibrarySystem/LibrarySystem/LibrarySystem/Program.cs
+++ LibrarySystem/LibrarySystem/LibrarySystem/Program.cs
@@ -6,10 +6,79 @@ namespace LibrarySystem
     {
         static void Main(string[] args)
         {
+            bool isRunning = true;
+
             Library library = new Library();
 
-
             Console.WriteLine(library.WelcomeMessage());
+
+            Console.WriteLine(" \nWhat do you want to do? \n ");
+
+            while (isRunning)
+            {
+
+                Console.WriteLine("1.Borrow a Book \n2.Return Borrowed Book \n3.Check for Borrowed Book \n4.Extend Time on Book \n5.Exit the Library \n");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Which book would you like to borrow?\n \n1. Bob and Bob \n2. Bob and Bob gets married \n3. Bob and Bob's Lifestory \n");
+                        string chooseBook = Console.ReadLine();
+                        bool borrowingBook = true;
+
+                        while (borrowingBook)
+                        {
+                            switch (chooseBook)
+                            {
+                                case "1":
+                                    library.BorrowBook1();
+                                    Console.WriteLine("You have borrowed: " + library.borrowedBook[0] + "\n");
+                                    borrowingBook = false;
+                                    break;
+
+                                case "2":
+                                    library.BorrowBook2();
+                                    Console.WriteLine("You have borrowed: " + library.borrowedBook[0] + "\n");
+                                    borrowingBook = false;
+                                    break;
+
+                                case "3":
+                                    library.BorrowBook3();
+                                    Console.WriteLine("You have borrowed: " + library.borrowedBook[0] + "\n");
+                                    borrowingBook = false;
+                                    break;
+
+                                default:
+                                    Console.WriteLine("Not a book \n");
+                                    break;
+                            }
+                        }
+                        break;
+
+                    case "2":
+                        Console.WriteLine("You have returned: " + library.borrowedBook[0] + "\n");
+                        library.borrowedBook[0] = "Nothing";
+                        break;
+
+                    case "3":
+
+                        break;
+
+                    case "4":
+
+                        break;
+
+                    case "5":
+                        Console.WriteLine("See you again soon!");
+                        isRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("I don't understand \n ");
+                        break;
+                }
+            }
         }
     }
 }
